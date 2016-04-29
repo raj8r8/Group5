b27b0246fe8904615c58733a0c12d3042573a62d
ryanftray <ryanftray@yahoo.com>
2016-04-29 11:07:52 -0500

    Update item_category_edit.php

--- item_category_edit.php
+++ item_category_edit.php
@@ -18,7 +18,7 @@
 				echo "<p>Debugging error: " . mysqli_connect_error() . PHP_EOL."</p>";
 				exit;
 			    }
-    $sql = "UPDATE item_category SET name = ?";
+    $sql = "UPDATE item_category SET name = ? WHERE id=$id";
     if($stmt = $db->prepare($sql)){
         
         $stmt->bind_param('s', $category);
