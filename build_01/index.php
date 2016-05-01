704b565d038046008c57a57d8fda4c9a92917bb1
Phaseshifter <pnyakoojo.16@gmail.com>
2016-05-01 17:45:43 -0500

    Added files via upload

--- build_01/index.php
+++ build_01/index.php
@@ -9,15 +9,14 @@
 <html>
 	<head>
 		<!--  I USE BOOTSTRAP BECAUSE IT MAKES FORMATTING/LIFE EASIER -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"><!-- Optional theme -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script><!-- Latest compiled and minified JavaScript -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 		<script>
 		$(document).ready(function(){
-	          //var original = $("#content").html();
 	          $("#header").load("header.php");
-	          $("#content").load("VerifyLogin.php");
+	          $("#content").load("login.php");
          });
 		</script>
 		<style>
