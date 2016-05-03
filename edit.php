6e56011983de0fa2c7dead9a038e6ced806f5d10
btomblinson <goalieboy17@gmail.com>
2016-05-02 19:38:44 -0500

    Make Edit Items work fully

--- edit.php
+++ edit.php
@@ -30,27 +30,87 @@ if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) { // if us
 
        <div id="content">
     <h1>Edit An Item</h1>
-            <form action="item_edit.php" id="edit">
-                <label>ID:</label><input type="text" name="id">
+            <form action="item_edit.php" id="edit" method="POST">
+                <label>ID:</label><input type="text" name="id" readonly="readonly" value="<?=$_POST["id"]?>">
                 <br/>
                <label>Name:</label><input type="text" name="name">
                  <br/>
                 <label>Condition:</label>
-            <select name="condition" form="edit">
-                <option value="1">Functional</option>
-                <option value="2">Broken</option>
-            </select>
+       <select class='form-control' name="item_condition" id= "item_condition">
+    <?php
+        $con= mysqli_connect("localhost","public","P@ssword","Project");
+        if (!$con) {
+            echo "<p>Error: Unable to connect to MySQL." . PHP_EOL."</p>";
+            echo "<p>Debugging errno: " . mysqli_connect_errno() . PHP_EOL."</p>";
+            echo "<p>Debugging error: " . mysqli_connect_error() . PHP_EOL."</p>";
+            exit;
+        }
+        
+        $query1 = "SELECT id,name FROM item_condition";
+        $statement1 = mysqli_stmt_init($con);
+        if (mysqli_stmt_prepare($statement1, $query1)) {
+            
+            /* execute query */
+            mysqli_stmt_execute($statement1);
+            if(mysqli_stmt_error($statement1) != ""){
+    											 
+                echo "<p>Error because of ".mysqli_stmt_error($statement1)."</p>";
+                exit;
+            }
+        }
+        else{
+            echo "<p>".mysqli_stmt_error($statement1)."</p>";
+            
+        }
+        $result1 = mysqli_stmt_get_result($statement1);
+        
+        
+        while($row1 = mysqli_fetch_array($result1, MYSQLI_NUM)){
+            
+            echo "<option value='".$row1[0]."'>".$row1[1]."</option>";
+            
+        }
+        mysqli_stmt_close($statement1);
+        ?>
+    </select>
                  <br/>
                 <label>Category:</label>
-            <select name="category" form="edit">
-                <option value="1">Mac</option>
-                <option value="2">PC</option>
-                <option value="3">Bike</option>
-                <option value="4">Mac Charger</option>
-                <option value="5">PC Charger</option>
-            </select>
+           <select class='form-control' name="item_category" id="item_category">
+        <?php
+            $con= mysqli_connect("localhost","public","P@ssword","Project");
+            if (!$con) {
+                echo "<p>Error: Unable to connect to MySQL." . PHP_EOL."</p>";
+                echo "<p>Debugging errno: " . mysqli_connect_errno() . PHP_EOL."</p>";
+                echo "<p>Debugging error: " . mysqli_connect_error() . PHP_EOL."</p>";
+                exit;
+            }
+            $query0 = "SELECT id,name FROM item_category";
+            $statement0 = mysqli_stmt_init($con);
+            if (mysqli_stmt_prepare($statement0, $query0)) {
+                
+                /* execute query */
+                mysqli_stmt_execute($statement0);
+                if(mysqli_stmt_error($statement0) != ""){
+        											 
+                    echo "<p>Error because of ".mysqli_stmt_error($statement0)."</p>";
+                    exit;
+                }
+            }
+            else{
+                echo "<p>".mysqli_stmt_error($statement0)."</p>";
+                
+            }
+            
+            $result = mysqli_stmt_get_result($statement0);
+            while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+                echo "<option value='".$row[0]."'>".$row[1]."</option>";
+                
+            }
+            mysqli_stmt_close($statement0); ?>
+        </select>
+        
                  <br/>
-            <input type="submit" value = "submit">
+            <input type="submit" name="submit" value="Submit">
             </form>
         </div>
     </body>
--- item_edit.php
+++ item_edit.php
@@ -6,40 +6,78 @@
 	    //exit;
 	}   
     if(isset($_POST['submit'])) {
-        $db = mysqli_connect("localhost", "public", "P@ssword", "Project")
-        or die("Connect Error");
+        $con = mysqli_connect("localhost", "public", "P@ssword", "Project");
         $id = htmlspecialchars($_POST['id']);
         $name = htmlspecialchars($_POST['name']);
-        $category = htmlspecialchars($_POST['category']);
-        $condition = htmlspecialchars($_POST['condition']);
+        $category = htmlspecialchars($_POST['item_category']);
+        $condition = htmlspecialchars($_POST['item_condition']);
     if (!$con) {
 				echo "<p>Error: Unable to connect to MySQL." . PHP_EOL."</p>";
 				echo "<p>Debugging errno: " . mysqli_connect_errno() . PHP_EOL."</p>";
 				echo "<p>Debugging error: " . mysqli_connect_error() . PHP_EOL."</p>";
 				exit;
 			    }
-     $sql = "UPDATE item SET name = ? WHERE id=$id";
-    if($stmt = $db->prepare($sql)){
-        
-        $stmt->bind_param('ss', $id, $name);
-    
-        $stmt->execute() or die("Execute error");
+     $sql = "UPDATE item SET name = ? WHERE id=?;";
+    		$statement1 = mysqli_stmt_init($con);
+    if (mysqli_stmt_prepare($statement1, $sql)) {
+                     
+                        /* bind parameters for markers */
+                        mysqli_stmt_bind_param($statement1, "sd",$name, $id);
+                        
+                        
+                        /* execute query */
+                        mysqli_stmt_execute($statement1);
+                        if(mysqli_stmt_error($statement1) != ""){
+     
+                                echo "<p>Error because of ".mysqli_stmt_error($statement1)."</p>";
+                                exit;
+                        }
+    }
+    else{
+            echo "<p>".mysqli_stmt_error($statement1)."</p>";
+            
     }
         
-    $sql2 = "UPDATE item_has_category SET name = ? WHERE item_id=$id";
-    if($stmt2 = $db->prepare($sql2)){
-        
-        $stmt2->bind_param('s', $category);
-    
-        $stmt2->execute() or die("Execute error");
+    $sql2 = "UPDATE item_has_category SET item_category_id = ? WHERE item_id=?;";
+    $statement2 = mysqli_stmt_init($con);
+    if (mysqli_stmt_prepare($statement2, $sql2)) {
+                     
+                        /* bind parameters for markers */
+                        mysqli_stmt_bind_param($statement2, "dd",$category,$id);
+                        
+                        
+                        /* execute query */
+                        mysqli_stmt_execute($statement2);
+                        if(mysqli_stmt_error($statement2) != ""){
+     
+                                echo "<p>Error because of ".mysqli_stmt_error($statement2)."</p>";
+                                exit;
+                        }
+    } 
+    else{
+            echo "<p>".mysqli_stmt_error($stmt2)."</p>";
+            
+    }  
+    $sql3 = "UPDATE item_condition_update SET item_condition_id = ? WHERE item_id= ?;";
+    $statement3 = mysqli_stmt_init($con);
+    if (mysqli_stmt_prepare($statement3, $sql3)) {
+                     
+                        /* bind parameters for markers */
+                        mysqli_stmt_bind_param($statement3, "dd",$condition,$id);
+                        
+                        
+                        /* execute query */
+                        mysqli_stmt_execute($statement3);
+                        if(mysqli_stmt_error($statement3) != ""){
+     
+                                echo "<p>Error because of ".mysqli_stmt_error($statement3)."</p>";
+                                exit;
+                        }
     }
-        
-    $sql3 = "UPDATE item_condition_update SET name = ? WHERE item_id=$id";
-    if($stmt3 = $db->prepare($sql3)){
-        
-        $stmt3->bind_param('s', $condition);
-    
-        $stmt3->execute() or die("Execute error");
+    else{
+            echo "<p>".mysqli_stmt_error($statement3)."</p>";
+            
     }
-    }
+   	header("Location: items.php"); 
+ }
 ?>
