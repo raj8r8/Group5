c3843cfab8cf133102ca7d50ac97a70c5818d7a4
raj8r8 <raj8r8@mail.missouri.edu>
2016-05-11 12:58:27 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) { // if user isn't logged in
+  header("Location: ./index.php"); // go to Login page
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+        <link href="header.css" rel="stylesheet">
+        <script src="header.js"></script>
+        <link href="//cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css" rel="stylesheet"></link>
+    </head>
+    <body>
+        <div id="header">
+        <div id="title">
+       <a href="home.php"><h1>Welcome to the Mizzou Item Management System</h1></a>
+        </div>
+            <ul id="header-items">
+              <li>Items
+                    
+                    <ul class="dropdown">
+                    <?php
+                    if(($_SESSION["level"])=="1") {
+                        echo "<li><a href='add_item.php'>Admin Rocks</li>";
+                        }
+                    else {
+                        echo '<li><a href="add_item.php">Add Item</li>';
+                    }
+                        
+                    ?>
+                    <li><a href="add_item.php">Add Item</li>
+                     <li><a href="transactions.php">All Transactions</a></li>
+                        <li><a href="checkin.php">Check In</a></li>
+                        <li><a href="checkout.php">Check Out</a></li>
+                        <li><a href="overdue.php">Overdue Items</a></li>
+                        <li><a href="items.php">View All Items</a></li>
+                    </ul>
+                      <span class="caret"></span> 
+                </li>
+                <li>Locations
+                    <ul class="dropdown">
+                        <li><a href="add_location.php">Add Location</a></li>
+                    </ul>
+                    <span class="caret"></span>
+                </li>
+                <li>Students
+                    <ul class="dropdown">
+                        
+                        <li><a href='addwaiver.php'>Add Waiver</a></li>
+                    </ul>
+                    <span class="caret"></span>
+                </li>
+                <?php
+                if(($_SESSION["level"])=="2") {
+                    echo '<li>Employees';
+                    echo '<ul class="dropdown">';
+                    echo '<li><a href="register.php">New Emloyees</a></li>';
+                    echo '</ul>';
+                    echo '<span class="caret"></span>';
+                    echo '</li>';
+                }
+                ?>
+                <li id="logout" class="nodropdown"><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </body>
+    
+</html>
