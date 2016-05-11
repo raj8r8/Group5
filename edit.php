a5d37350ca5a35538becd3d6620d2e095303fa42
raj8r8 <raj8r8@mail.missouri.edu>
2016-05-11 13:01:43 -0500

    Update edit.php

--- edit.php
+++ edit.php
@@ -11,7 +11,7 @@ if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) { // if us
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script>
         $(document).ready(function(){
-                    $("#loader").load("header.html");            
+                    $("#loader").load("header.php");            
              });
         </script>
         <style> 
