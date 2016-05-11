acca71b7a392024df2d6f773c69d16aa7ce93e40
jaylee109 <jaylee109@yahoo.com>
2016-05-11 14:01:06 -0500

    Add files via upload

---
+++ deleteitem.php
@@ -0,0 +1,21 @@
+<?php
+$link=mysqli_connect("localhost","public","p@ssword","Project");
+if(isset($_POST["id"])){
+$itemid=$_POST['id'];
+$query = "DELETE FROM items WHERE id={$_POST['id']} LIMIT 1";
+
+mysql_query ($query,$link);
+
+if (mysql_affected_rows() == 1) {
+
+?>
+
+    <strong>Contact Has Been Deleted</strong><br/><br/>
+    <?php
+} else {
+  ?>
+
+  <strong>Failed to delete item</strong><br/><br/>
+  <?php
+}
+?>
